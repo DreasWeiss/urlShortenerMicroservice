a5c533f0bc94335567f9c72bbdd3fd2a578e2044
DreasWeiss <dreasweiss@gmail.com>
2022-12-30 14:06:55 +0300

    added body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
@@ -13,6 +14,8 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
