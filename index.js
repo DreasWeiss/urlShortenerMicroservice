80eed17bf1a3d2b5b9390032a75b9331b734ded3
DreasWeiss <dreasweiss@gmail.com>
2023-01-03 21:30:14 +0300

    added dns module, added postcheckingurl, added get api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const dns = require('dns');
 const cors = require('cors');
 const app = express();
 
@@ -11,17 +12,66 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+const urlShortenerList = [
+  {
+    "original_url": 'www.google.com',
+    "short_url": 1
+  },
+  {
+    "original_url": 'www.abc.com',
+    "short_url": 2
+  },
+  {
+    "original_url": 'www.ya.ru',
+    "short_url": 3
+  },
+
+];
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  const shortUrl = urlShortenerList.length + 1;
+
+  dns.lookup(url, (err, value) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      urlShortenerList.push({
+        "original_url": url,
+        "short_url": shortUrl
+      });
+      console.log(urlShortenerList);
+      res.json({
+        "original_url": url,
+        "short_url": shortUrl
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:new', (req, res) => {
+  let testShortUrl = req.params.new;
+  console.log(urlShortenerList);
+  let urlIndex = urlShortenerList.findIndex(i => i.short_url == testShortUrl);
+  console.log(urlIndex);
+  if (urlIndex >= 0){
+    res.redirect(`https://${urlShortenerList[urlIndex].original_url}`);
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
