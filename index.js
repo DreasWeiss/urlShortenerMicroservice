89b27d6328b86e94468b06f9e9a9fe23f27e90eb
DreasWeiss <dreasweiss@gmail.com>
2023-01-04 00:44:30 +0300

    some checker to pass the test on fcc

--- index.js
+++ index.js
@@ -25,42 +25,36 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-const urlShortenerList = [
-  {
-    "original_url": 'www.google.com',
-    "short_url": 1
-  },
-  {
-    "original_url": 'www.abc.com',
-    "short_url": 2
-  },
-  {
-    "original_url": 'www.ya.ru',
-    "short_url": 3
-  },
-
-];
+const urlShortenerList = [];
 
 app.post('/api/shorturl', (req, res) => {
   let url = req.body.url;
   const shortUrl = urlShortenerList.length + 1;
-  url = url.replace(/^https:\/\/|^http:\/\//, '');
+  let validUrl = url.replace(/^https:\/\/|^http:\/\//, '');
 
-  dns.lookup(url, (err, value) => {
-    if (err) {
-      res.json({ error: 'invalid url' });
-    } else {
-      urlShortenerList.push({
-        "original_url": url,
-        "short_url": shortUrl
-      });
-      console.log(urlShortenerList);
-      res.json({
-        "original_url": url,
-        "short_url": shortUrl
-      });
-    }
-  });
+  if (validUrl === ''){
+    res.json({ error: 'invalid url' });
+  }
+
+  else if (url.match(/^https:\/\/|^http:\/\//)){
+    dns.lookup(validUrl, (err) => {
+      if (err) {
+        res.json({ error: 'invalid url' });
+      } else {
+        urlShortenerList.push({
+          "original_url": url,
+          "short_url": shortUrl
+        });
+        console.log(urlShortenerList);
+        res.json({
+          "original_url": url,
+          "short_url": shortUrl
+        });
+      }
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 });
 
 app.get('/api/shorturl/:new', (req, res) => {
@@ -69,7 +63,7 @@ app.get('/api/shorturl/:new', (req, res) => {
   let urlIndex = urlShortenerList.findIndex(i => i.short_url == testShortUrl);
   console.log(urlIndex);
   if (urlIndex >= 0){
-    res.redirect(`https://${urlShortenerList[urlIndex].original_url}`);
+    res.redirect(urlShortenerList[urlIndex].original_url);
   } else {
     res.json({ error: 'invalid url' });
   }
