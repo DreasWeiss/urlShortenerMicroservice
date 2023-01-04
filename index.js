e682ccc2f03d0a0342ab58b8b576921d5fdae85a
DreasWeiss <dreasweiss@gmail.com>
2023-01-04 14:26:58 +0300

    right answer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const dns = require('dns');
+// const bodyParser = require('body-parser');
+// const dns = require('dns');
 // const mongodb = require('mongodb');
 // const mongoose = require('mongoose');
 const cors = require('cors');
@@ -11,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,55 +19,57 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 // Your first API endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+// {
+//   "original_url": "https://www.google.com",
+//   "short_url": 1
+// }
 
-let links = [];
-let id = 0;
+const originalUrls = [];
+const shortUrls = [];
 
 app.post('/api/shorturl', (req, res) => {
-  const {url} = req.body;
-  console.log(url);
-  const regex = /^https?:\/\/[0-9a-z]*/i;
+  const url = req.body.url;
+  const foundIndex = originalUrls.indexOf(url);
 
-  if (!regex.exec(url)){
-    return res.json({ error: 'invalid url' });
-  } else {
-    dns.lookup(url.replace(/^https?:\/\//i, ''), (err) => {
-    if (err) {
-      return res.json({ error: 'invalid url' });
-    } else {
-      id++;
-      let link = {
-        original_url : url, 
-        short_url : id-1
-      };
-    
-      links.push(link);
-    
-      res.json(link);
-    }
-  });
+  if(!url.includes('https://') && !url.includes('http://')){
+    return res.json({
+      error: 'invalid url'
+    });
   }
+
+  if(foundIndex<0){
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
+    });
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  });
 });
 
-// app.get('/api/shorturl/:id', (req, res) => {
-//   // const {id} = req.params;
-//   // const link = links.find(l=>l.short_url === `${id}`);
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const shorturl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shorturl);
 
-//   // if(link){
-//   //   return res.redirect(link.original_url);
-//   // } else {
-//   //   return res.json({
-//   //     error: 'No short url'
-//   //   });
-//   // }
-// });
+  if(foundIndex < 0){
+    return res.json({
+      error: "No short URL found for the given input"
+    });
+  }
 
+  res.redirect(originalUrls[foundIndex]);
+
+});
 
 
 app.listen(port, function () {
